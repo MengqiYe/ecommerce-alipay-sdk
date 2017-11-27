81b8a7720a9a693d65dd79be4a5942ea372bb213
Mengqi Ye <mengqi.ye@yungoal.com>
2017-11-27 15:35:50 +0800

    Rename

--- ecommerce_alipay_sdk/config.py
+++ ecommerce_alipay_sdk/config.py
@@ -1,6 +1,6 @@
-__version__ = "0.1"
+__version__ = "0.0.1"
 __pypi_username__ = "seaswander"
-__pypi_packagename__ = "alipay_sdk"
+__pypi_packagename__ = "ecommerce_alipay_sdk"
 __github_username__ = "seaswander"
 __github_reponame__ = "ecommerce-alipay-sdk"
 __endpoint_map__ = {
